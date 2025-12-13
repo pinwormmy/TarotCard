dc6e4c013c3ba98e2f9e9e1fd20b21bbee64c8b6
pinwormmy <ozkhan@naver.com>
2025-12-13 17:02:45 +0900

    구글플레이 업로드시 API35 이상 요구해서 버전바꿈
    버전만 바꿈. 구글플레이에 테스트 버전만 올린 상태.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,10 +14,10 @@ android {
         applicationId = "com.pinwormmy.midoritarot"
         minSdk = 24
         //noinspection OldTargetApi
-        targetSdk = 34
+        targetSdk = 35
 
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
